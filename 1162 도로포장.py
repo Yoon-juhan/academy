e1e2ae9aa854aa38a53383fa7c99f6f733d4db4a
Yoon-juhan <yoonjuhan00@gmail.com>
2024-01-09 14:40:39 +0900

    도로포장

--- 1162 도로포장.py
+++ 1162 도로포장.py
@@ -12,28 +12,29 @@ for _ in range(m):
     graph[a].append((c, b))
     graph[b].append((c, a))
 
-INF = int(1e9)
+INF = int(1e12)
 
+distance = [[INF] * (k+1) for _ in range(n+1)]
+distance[1][0] = 0
 
-distance = [[INF] * (n+1) for _ in range(n+1)]
-distance[1][1] = 0
-
-q = [(0, 1, 1)]
+q = [(0, 1, 0)]
 
 while q:
     now_d, now, cnt = heappop(q)
 
-    if distance[cnt][now] < now_d: 
+    if distance[now][cnt] < now_d: 
         continue
 
     for next_d, next in graph[now]:
         new_d = now_d + next_d
-        if distance[cnt][next] > new_d:
-            distance[cnt][next] = new_d
+        if distance[next][cnt] > new_d:
+            distance[next][cnt] = new_d
             heappush(q, (new_d, next, cnt))
-        if cnt+1 <= k+1 and distance[cnt+1][next] > now_d:
-            distance[cnt+1][next] = now_d
+        if cnt+1 <= k and distance[next][cnt+1] > now_d:
+            distance[next][cnt+1] = now_d
             heappush(q, (now_d, next, cnt+1))
 
 for i in distance:
     print(i)
+
+print(min(distance[-1]))
