067d363c3bc67cb74f07245bab750dc845e56ef8
Yoon-juhan <108213439+Yoon-juhan@users.noreply.github.com>
2024-01-20 17:27:15 +0900

    Add files via upload

--- 0120/1520 내리막길.py
+++ 0120/1520 내리막길.py
@@ -1,35 +1,27 @@
-n, m = map(int, input().split())
-
-MAP = [list(map(int, input().split())) for _ in range(n)]
-
-for i in MAP:
-    print(i)
+import sys
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
 
 def dfs(x, y):
-    
+
+    if x == m-1 and y == n-1:
+        return 1
+
     if dp[x][y] != -1:
         return dp[x][y]
     
-    if x == n and y == m:
-        dp[x][y] = 1
-        return dp[x][y]
-
     dp[x][y] = 0
 
     for i in range(4):
-        nx, ny = x + mx[i], y + my[i]
-
-        if 0 <= nx < n and 0 <= ny < m and MAP[x][y] > MAP[nx][ny]:
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n and MAP[x][y] > MAP[nx][ny]:
             dp[x][y] += dfs(nx, ny)
     
     return dp[x][y]
-            
 
-dp = [[-1] * m for _ in range(n)]
+m, n = map(int, input().split())
+MAP = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
+dx, dy = [1,-1,0,0], [0,0,1,-1]
 
-mx, my = [0, 0, 1, -1], [1, -1, 0, 0]
-
-dfs(0, 0)
-
-for i in dp:
-    print(i)
+print(dfs(0,0))
--- 0120/1948 임계경로.py
+++ 0120/1948 임계경로.py
@@ -5,3 +5,10 @@ input = sys.stdin.readline
 n = int(input())
 m = int(input())
 
+graph = defaultdict(list)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+start, end = map(int, input().split())
